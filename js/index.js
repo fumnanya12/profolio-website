39ee13f2b3b36b06ecf000f7b50bb12d85b8e035
Fumnanya Obi <fumnanya.obi@gmail.com>
2022-10-23 21:28:11 -0500

    Website update
    Made a dark and light mode

--- js/index.js
+++ js/index.js
@@ -10,3 +10,30 @@ navLink.forEach(link =>{
         document.body.classList.remove('nav-open');
     })
 })
+
+
+/*========Dark light=====*/
+const themeButton=document.getElementById('theme-button');
+const darktheme='dark-theme';
+const iconTheme='fa-moon';
+
+const selectedTheme= localStorage.getItem('selected-theme')
+const selectedIcon=localStorage.getItem('selected-icon')
+
+const getCurrentTheme=()=> document.body.classList.contains(darktheme) ? 'dark':'light'
+const getCurrentIcon=()=>themeButton.classList.contains(iconTheme) ? 'fa-sun':'fa-moon'
+
+if (selectedTheme){
+    document.body.classList[selectedTheme ==='dark' ? 'add' : 'remove'](darktheme)
+    themeButton.classList[selectedIcon ==='fa-moon'? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click',() =>{
+console.log(iconTheme)
+    document.body.classList.toggle(darktheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme',getCurrentTheme())
+    localStorage.setItem('selected-icon',getCurrentIcon())
+
+
+})
